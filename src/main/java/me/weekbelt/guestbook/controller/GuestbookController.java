91893bbe0a68f294abee9fd40168ff6fc9405141
weekbelt <vfrvfr4207@gmail.com>
2020-12-31 07:54:51 +0900

    feat: Create GuestbookController

---
+++ src/main/java/me/weekbelt/guestbook/controller/GuestbookController.java
@@ -0,0 +1,20 @@
+package me.weekbelt.guestbook.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@Slf4j
+public class GuestbookController {
+
+    @GetMapping({"/", "/list"})
+    public String list() {
+        log.info("list ............");
+
+        return "/guestbook/list";
+    }
+}
