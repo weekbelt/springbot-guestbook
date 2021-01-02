0b83e4e6959db2ddc4c6d6a9054bb6d1c91af8b5
weekbelt <vfrvfr4207@gmail.com>
2021-01-02 15:28:18 +0900

    feat: Create guestbook list handler & page

--- src/main/java/me/weekbelt/guestbook/controller/GuestbookController.java
+++ src/main/java/me/weekbelt/guestbook/controller/GuestbookController.java
@@ -1,20 +1,30 @@
 package me.weekbelt.guestbook.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.weekbelt.guestbook.dto.PageRequestDTO;
+import me.weekbelt.guestbook.service.GuestbookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Slf4j
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
-        log.info("list ............");
+    private final GuestbookService guestbookService;
 
-        return "/guestbook/list";
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("list ..................." + pageRequestDTO);
+        model.addAttribute("result", guestbookService.getList(pageRequestDTO));
     }
 }
