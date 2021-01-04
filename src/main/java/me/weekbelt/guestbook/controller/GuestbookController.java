282f3132d65a9c1727d531c5f14c4ac8eb6cd913
weekbelt <vfrvfr4207@gmail.com>
2021-01-04 22:04:49 +0900

    feat: Create regist page and process regist

--- src/main/java/me/weekbelt/guestbook/controller/GuestbookController.java
+++ src/main/java/me/weekbelt/guestbook/controller/GuestbookController.java
@@ -2,12 +2,15 @@ package me.weekbelt.guestbook.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.weekbelt.guestbook.dto.GuestbookDTO;
 import me.weekbelt.guestbook.dto.PageRequestDTO;
 import me.weekbelt.guestbook.service.GuestbookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -26,5 +29,21 @@ public class GuestbookController {
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         log.info("list ..................." + pageRequestDTO);
         model.addAttribute("result", guestbookService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+
+        // 새로 추가된 엔티티의 번호
+        Long gno = guestbookService.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
     }
 }
